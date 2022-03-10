4267be073cbc9dee4e2bd5ca46135e1f3064cbdd
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-03-10 21:33:49 +0800

    Reject unknown propositions

--- src/main/scala/com/melvic/chi/Fault.scala
+++ src/main/scala/com/melvic/chi/Fault.scala
@@ -1,16 +1,28 @@
 package com.melvic.chi
 
 import com.melvic.chi.ast.Proposition
+import com.melvic.chi.ast.Proposition.Atom
 
 sealed trait Fault
 
 object Fault {
   final case class CannotProve(proposition: Proposition) extends Fault
   final case class ParseError(msg: String) extends Fault
+  final case class UnknownPropositions(identifiers: List[Atom]) extends Fault
 
   def cannotProve(proposition: Proposition): Fault =
     CannotProve(proposition)
 
   def parseError(msg: String): Fault =
     ParseError(msg)
+
+  def show(fault: Fault): String =
+    fault match {
+      case CannotProve(proposition) =>
+        val propositionString = Proposition.show(proposition)
+        s"Can not prove the following proposition: $propositionString"
+      case ParseError(msg) => s"Parse Error: $msg"
+      case UnknownPropositions(identifiers) =>
+        s"Unknown propositions: ${identifiers.map(Proposition.show).mkString(", ")}"
+    }
 }
--- src/main/scala/com/melvic/chi/ast/Proposition.scala
+++ src/main/scala/com/melvic/chi/ast/Proposition.scala
@@ -3,19 +3,52 @@ package com.melvic.chi.ast
 sealed trait Proposition
 
 object Proposition {
-  sealed trait PUnit extends Proposition
+  sealed trait Atom extends Proposition {
+    def value: String
+  }
+
+  object Atom {
+    def unapply(atom: Atom): Option[String] =
+      Some(atom.value)
+  }
+
+  sealed trait PUnit extends Atom {
+    override def value = "()"
+  }
+
   case object PUnit extends PUnit
-  final case class Atom(value: String) extends Proposition
+  final case class Identifier(value: String) extends Atom
   final case class Conjunction(components: List[Proposition]) extends Proposition
   final case class Disjunction(left: Proposition, right: Proposition) extends Proposition
   final case class Implication(antecedent: Proposition, consequent: Proposition) extends Proposition
 
+  def fold[A](proposition: Proposition, init: A)(f: (A, Atom) => A): A =
+    proposition match {
+      case atom: Atom => f(init, atom)
+      case Conjunction(components) =>
+        components.foldLeft(init) { (acc, proposition) =>
+          fold(proposition, acc)(f)
+        }
+      case Disjunction(left, right) =>
+        fold(right, fold(left, init)(f))(f)
+      case Implication(antecedent, consequent) =>
+        fold(consequent, fold(antecedent, init)(f))(f)
+    }
+
+  def filter(proposition: Proposition)(f: Atom => Boolean): List[Atom] =
+    fold(proposition, List.empty[Atom]) { (acc, atom) =>
+      if (f(atom)) atom :: acc
+      else acc
+    }
+
+  def exists(proposition: Proposition)(f: Atom => Boolean): Boolean =
+    filter(proposition)(f).nonEmpty
+
   def show(proposition: Proposition): String =
     proposition match {
-      case PUnit => "()"
-      case Atom(value) => value
-      case Conjunction(components) => "(" + components.map(show).mkString(", ") + ")"
-      case Disjunction(left, right) => s"Either[${show(left)}, ${show(right)}]"
+      case Atom(value)                         => value
+      case Conjunction(components)             => "(" + components.map(show).mkString(", ") + ")"
+      case Disjunction(left, right)            => s"Either[${show(left)}, ${show(right)}]"
       case Implication(antecedent, consequent) => s"(${show(antecedent)} => ${show(consequent)})"
     }
 }
