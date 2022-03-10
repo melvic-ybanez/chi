6e7078857ea7f3242380b7ea45a8bb212a3af080
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-03-11 03:20:45 +0800

    Add body pretty printing

--- src/main/scala/com/melvic/chi/ast/Proof.scala
+++ src/main/scala/com/melvic/chi/ast/Proof.scala
@@ -4,25 +4,38 @@ sealed trait Proof
 
 //noinspection SpellCheckingInspection
 object Proof {
-  sealed trait TUnit                                                      extends Proof
-  case object TUnit                                                       extends TUnit
-  final case class Variable(name: String, proposition: Proposition)       extends Proof
-  final case class Conjunction(components: List[Proof])                   extends Proof
-  final case class PRight(proof: Proof)                                   extends Proof
-  final case class PLeft(proof: Proof)                                    extends Proof
-  final case class Abstraction(domain: Proof, codomain: Proof)            extends Proof
+  sealed trait TUnit extends Proof
+  case object TUnit extends TUnit
+  final case class Variable(name: String, proposition: Proposition) extends Proof
+  final case class Conjunction(components: List[Proof]) extends Proof
+  final case class PRight(proof: Proof) extends Proof
+  final case class PLeft(proof: Proof) extends Proof
+  final case class Abstraction(domain: Proof, codomain: Proof) extends Proof
   final case class Application(functionName: String, params: List[Proof]) extends Proof
 
-  def show(proof: Proof): String =
-    proof match {
-      case TUnit                             => "()"
-      case Variable(name, _)                 => name
-      case Conjunction(terms)                => "(" + terms.map(Proof.show).mkString(", ") + ")"
-      case PRight(term)                      => s"Right(${Proof.show(term)})"
-      case PLeft(term)                       => s"Left(${Proof.show(term)})"
+  def show(proof: Proof, level: Option[Int]): String = {
+    val singleIndent = "  "
+    val indent = level.map(singleIndent * _).getOrElse("")
+    val nextLine = level.map(_ => "\n").getOrElse("")
+    val nextLevel = level.map(_ + 1)
+    val proofString = proof match {
+      case TUnit             => "()"
+      case Variable(name, _) => name
+      case Conjunction(terms) =>
+        val termsString = terms
+          .map(Proof.show(_, nextLevel))
+          .mkString(", " + nextLine)
+        "(" + nextLine + termsString + nextLine + indent + ")"
+      case PRight(term) => s"Right(${Proof.show(term, None)})"
+      case PLeft(term)  => s"Left(${Proof.show(term, None)})"
       case Abstraction(params: Conjunction, codomain) =>
-        s"{ case ${show(params)} =>\n    ${show(codomain)}\n  }"
-      case Abstraction(domain, codomain)     => s"${Proof.show(domain)} => ${Proof.show(codomain)}"
-      case Application(functionName, params) => s"$functionName(${params.map(Proof.show).mkString(", ")})"
+        val bodyIndent = if (indent.nonEmpty) indent + singleIndent else singleIndent * 2
+        s"{ case ${show(params, None)} =>\n$bodyIndent${show(codomain, None)}\n  }"
+      case Abstraction(domain, codomain) =>
+        s"${Proof.show(domain, None)} => $nextLine${Proof.show(codomain, nextLevel)}"
+      case Application(functionName, params) =>
+        s"$functionName($nextLine${params.map(Proof.show(_, nextLevel)).mkString(", " + nextLine)}$nextLine$indent)"
     }
+    s"$indent$proofString"
+  }
 }
