c197e206dc9a74f2022a5e839c805e914078009e
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-04-02 19:06:04 +0800

    Add editor menu

--- src/main/scala/com/melvic/chi/views/MenuBarView.scala
+++ src/main/scala/com/melvic/chi/views/MenuBarView.scala
@@ -4,13 +4,29 @@ import java.awt.event.{ActionEvent, KeyEvent}
 import javax.swing.{JMenu, JMenuBar, JMenuItem, KeyStroke}
 
 class MenuBarView extends JMenuBar {
-  val fileMenu = {
-    val fileMenu = new JMenu("File")
-    val exitMeuItem = new JMenuItem("Exit")
-    exitMeuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK))
-    fileMenu.add(exitMeuItem)
-    fileMenu
+  val fileMenu = createMenuItem(
+    new JMenu("File"),
+    "Exit",
+    Some(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK))
+  )
+
+  val editorMenu = createMenuItem(
+    createMenuItem(
+      new JMenu("Editor"),
+      "Run",
+      Some(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK))
+    ),
+    "Clear",
+    None
+  )
+
+  def createMenuItem(menu: JMenu, itemName: String, keyStroke: Option[KeyStroke] = None): JMenu = {
+    val menuItem = new JMenuItem(itemName)
+    keyStroke.foreach(menuItem.setAccelerator)
+    menu.add(menuItem)
+    menu
   }
 
   add(fileMenu)
+  add(editorMenu)
 }
