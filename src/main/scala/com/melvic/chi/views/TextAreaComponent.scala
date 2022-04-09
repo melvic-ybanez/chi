24080745cfb935c57010235b7d5943812bbf76d1
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-04-10 03:37:24 +0800

    Enable anti-aliasing

--- src/main/scala/com/melvic/chi/views/TextAreaComponent.scala
+++ src/main/scala/com/melvic/chi/views/TextAreaComponent.scala
@@ -3,19 +3,17 @@ package com.melvic.chi.views
 import com.melvic.chi.Config
 import org.fife.ui.rsyntaxtextarea.{RSyntaxTextArea, SyntaxConstants, Theme}
 import org.fife.ui.rtextarea.RTextScrollPane
-import os.{Path, ResourcePath}
 
-import java.awt.{Font, GraphicsEnvironment}
+import java.awt.Font
 import java.awt.font.TextAttribute
-import java.io.{File, FileInputStream, IOException}
-import javax.swing.JTextArea
+import java.io.IOException
 import scala.collection.mutable
-import scala.io.Source
 import scala.jdk.CollectionConverters._
 
 class TextAreaComponent extends RSyntaxTextArea(50, 50) {
   setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_SCALA)
   setCodeFoldingEnabled(true)
+  setAntiAliasingEnabled(true)
 
   updateStyle()
   updateFont()
@@ -34,9 +32,8 @@ class TextAreaComponent extends RSyntaxTextArea(50, 50) {
 
     val newFont = font.getAttributes.asScala match {
       case attributes: mutable.Map[TextAttribute, Any] =>
-        attributes.put(TextAttribute.SIZE, 16)
         attributes.put(TextAttribute.LIGATURES, TextAttribute.LIGATURES_ON)
-        font.deriveFont(attributes.asJava)
+        font.deriveFont(attributes.asJava).deriveFont(16: Float)
       case _ => font
     }
     setFont(newFont)
