ed2b1845386ccf3a495ae57cc5e2b2d1384626c5
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-04-21 00:53:50 +0800

    Fix rendering of function application to a lambda

--- src/test/scala/com/melvic/chi/tests/HaskellFunctionsSpec.scala
+++ src/test/scala/com/melvic/chi/tests/HaskellFunctionsSpec.scala
@@ -19,4 +19,39 @@ class HaskellFunctionsSpec extends BaseSpec {
   "fst" should "return the first element" in {
     test("fst :: (a, b) -> a", "fst (a, b) = a")
   }
+
+  "snd" should "return the second element" in {
+    test("snd :: (a, b) -> b", "snd (a, b) = b")
+  }
+
+  "compose" should "apply the first function after the second" in {
+    test("compose :: (b -> c) -> (a -> b) -> a -> c", "compose f g a = f (g a)")
+  }
+
+  "andThen" should "apply the first function before the second" in {
+    test("andThen :: (a -> b) -> (b -> c) -> a -> c", "andThen f g a = g (f a)")
+  }
+
+  "unit" should "be provable with the universal value ()" in {
+    test("unit :: ()", "unit = ()")
+  }
+
+  "conjunction" should "depend on the proofs of its components" in {
+    test("foo :: a -> (b -> (a, b)) -> b -> (a, b)", "foo a f b = (a, b)")
+  }
+
+  "either" should "default to left when the evaluation succeeds" in {
+    test("left :: a -> Either a a", "left a = Left a")
+    test("left :: a -> Either a b", "left a = Left a")
+    test("right :: b -> Either a b", "right b = Right b")
+  }
+
+  "all assumptions" should "be considered" in {
+    test("foo :: (a -> c) -> (b -> c) -> b -> c", "foo f g b = g b")
+    test("foo :: (b -> c) -> (a -> c) -> b -> c", "foo f g b = f b")
+  }
+
+  "implication" should "evaluate it's antecedent recursive" in {
+    test("foo :: (a -> b) -> ((a -> b) -> c) -> c", "foo f g = g (\\a -> f a)")
+  }
 }
