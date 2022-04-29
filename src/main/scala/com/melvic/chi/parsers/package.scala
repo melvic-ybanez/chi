bb5ae16ea95f1185d4d8ebf8d525f8e985e88026
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-04-30 00:36:36 +0800

    Disable python parser

--- src/main/scala/com/melvic/chi/parsers/package.scala
+++ src/main/scala/com/melvic/chi/parsers/package.scala
@@ -8,7 +8,6 @@ package object parsers {
   def parseLanguageSignature(signature: String): ParseSignature =
     JavaParser
       .parseSignature(signature)
-      .orElse(PythonParser.parseSignature(signature))
       .orElse(HaskellParser.parseSignature(signature))
       .orElse(ScalaParser.parseSignature(signature))
 
